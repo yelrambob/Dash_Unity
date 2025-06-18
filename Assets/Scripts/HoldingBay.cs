c22f862044bbf3e091822ec54294544e2f8c229c
yelrambob <sean.chinery@gmail.com>
2025-06-18 12:27:37 -0400

    Update HoldingBay.cs

--- Assets/Scripts/HoldingBay.cs
+++ Assets/Scripts/HoldingBay.cs
@@ -1,30 +1,8 @@
-// Scripts/HoldingBay.cs
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HoldingBay : MonoBehaviour
+public class HoldingBay
 {
-    public Transform slotContainer;
-    public GameObject patientUIPrefab;
-    public int maxSlots = 4;
-
-    private List<Patient> currentPatients = new List<Patient>();
-
-    public void AddPatient(Patient patient)
-    {
-        if (currentPatients.Count >= maxSlots) return;
-
-        currentPatients.Add(patient);
-        GameObject ui = Instantiate(patientUIPrefab, slotContainer);
-        ui.GetComponent<PatientUI>().SetPatient(patient);
-    }
-
-    public void ClearBay()
-    {
-        currentPatients.Clear();
-        foreach (Transform child in slotContainer)
-        {
-            Destroy(child.gameObject);
-        }
-    }
+    public int MaxSlots;
+    public int OverflowSlots;
+    public List<Patient> Patients;
+    // Separate instance for CT3 holding bay
+    // Methods to add/remove patients, check overflow
 }
