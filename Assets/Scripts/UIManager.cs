b8ae62dd4fa115da0a59d36dd199852d8b278888
yelrambob <sean.chinery@gmail.com>
2025-06-18 12:28:10 -0400

    Create UIManager.cs

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,5 @@
+public class UIManager : MonoBehaviour
+{
+    // References to UI lists, scanner panels, timers, score displays
+    // Methods to update UI when patients move, scan completes, etc.
+}
