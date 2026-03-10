222b7df0a095f6e9afe5f78d648a5903f3dee85c
yelrambob <sean.chinery@gmail.com>
2026-03-10 12:41:47 -0400

    Create transport.py

---
+++ classes/transport.py
@@ -0,0 +1,5 @@
+      # Transport class — arrival delay range,
+      # hold wait range, leaving delay range,
+      # chance of long delay for each.
+      # States: WaitingAssignment, Acknowledged,
+      #         OnWay, Delivered
